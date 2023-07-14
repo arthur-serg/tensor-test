2d7f2ff896cd07f6fa15526bbec45c5864d7159b
exponenta.euler <exponenta.euler@gmail.com>
2023-07-14 18:41:46 +0500

    yandex image search manager

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 from yandex_pages import SearchManager
+from yandex_pages import ImageSearchManager
 
 
 def test_ya_search(browser):
@@ -10,3 +11,18 @@ def test_ya_search(browser):
     ya_main_page.press_enter_key()
     ya_main_page.check_search_page()
     ya_main_page.check_search_result()
+
+
+def test_yandex_images(browser):
+    image_search = ImageSearchManager(browser)
+    image_search.go_to_site()
+    image_search.check_menu_button()
+    image_search.open_menu_click_images()
+    image_search.check_images_url()
+    image_search.open_first_category()
+    image_search.check_search_field_text()
+    image_search.open_and_check_image()
+    image_search.navigate_forward()
+    image_search.check_is_image_changed()
+    image_search.navigate_backwards()
+    image_search.check_image()
