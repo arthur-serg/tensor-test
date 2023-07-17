c90e2f6386494ee05beb360c6660601f7191c2d1
exponenta.euler <exponenta.euler@gmail.com>
2023-07-17 20:57:02 +0500

    wip

--- yandex_pages.py
+++ yandex_pages.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.keys import Keys
 
 class YaSearchLocators:
     LOCATOR_YA_SEARCH_FIELD = (By.XPATH, "//*[@id=\"text\"]")
-    LOCATOR_YA_SEARCH_INPUT = (By.CLASS_NAME, "search2__input")
+    LOCATOR_YA_SEARCH_INPUT = (By.CSS_SELECTOR, ".search2__input .input__control")
     LOCATOR_YA_SEARCH_BUTTON = (By.CLASS_NAME, ".search3__button")
     LOCATOR_YA_SUGGEST_TABLE = (By.CLASS_NAME, "mini-suggest__popup-content")
     LOCATOR_YA_NAVIGATION_BAR = (By.CSS_SELECTOR, ".service__name")
@@ -16,8 +16,8 @@ class YaSearchLocators:
     LOCATOR_YA_MENU_BUTTON = (By.CLASS_NAME, "services-suggest__item-more")
     LOCATOR_YA_IMAGES_BUTTON = (By.CSS_SELECTOR, ".services-more-popup__section-all a[aria-label='Картинки']")
     LOCATOR_YA_IMAGES_LINK = (By.PARTIAL_LINK_TEXT, "yandex.ru/images")
-    LOCATOR_YA_IMAGES_FIRST_CATEGORY = (By.CLASS_NAME, "PopularRequestList-Item.PopularRequestList-Item_pos_0")
-    LOCATOR_YA_SEARCH_TEXT = (By.CLASS_NAME,"PopularRequestList-SearchText" )
+    LOCATOR_YA_IMAGES_FIRST_CATEGORY = (By.CSS_SELECTOR, ".PopularRequestList-Item.PopularRequestList-Item_pos_0")
+    LOCATOR_YA_SEARCH_TEXT = (By.CLASS_NAME,".PopularRequestList-Item.PopularRequestList-Item_pos_0 .PopularRequestList-SearchText")
 
 class SearchManager(BasePage):
 
@@ -59,7 +59,7 @@ class ImageSearchManager(BasePage):
         menu_button.click()
         images_button = self.find_element(YaSearchLocators.LOCATOR_YA_IMAGES_BUTTON)
         images_button.click()
-        time.sleep(5)
+        # time.sleep(5)
         assert images_button is not None
 
     def check_images_url(self):
@@ -74,12 +74,9 @@ class ImageSearchManager(BasePage):
 
     def check_search_field_text(self):
         target_images = self.find_element(YaSearchLocators.LOCATOR_YA_IMAGES_FIRST_CATEGORY)
-
         search_field = self.find_element(YaSearchLocators.LOCATOR_YA_SEARCH_INPUT)
-        category_title = target_images.text
-
-
-
+        category_title = target_images.get_attribute('innerText')
+        assert search_field.get_attribute('value') == category_title
 
 
     def open_and_check_image(self):
